7a662ec9048d350acbea4a1463765fba72ad8089
triskelion16 <triskelion.work@gmail.com>
2016-07-10 12:19:22 +0200

    Prework

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Serwis komputerowy</title>
+    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    
+
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/app.js" type="text/javascript"></script>
+</body>
+</html>
