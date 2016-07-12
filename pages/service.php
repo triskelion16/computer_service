4599425417c3a1ffe952741af7685e896ec59703
triskelion16 <triskelion.work@gmail.com>
2016-07-12 20:18:40 +0200

    add: servicer-landing page

--- pages/service.php
+++ pages/service.php
@@ -1,12 +1,13 @@
+<?php
+require("controlers/login_ctr.php");
+?>
+
 <main>
     <div class="main_width">
-        <div class="cl_print">
-            <?php
-            
-            require("controlers/login_ctr.php");
-            echo 'SEB!';
-            
-            ?>
+        <div class="sv_buttons">
+            <a href="?page=new_acc.php" class="home_a"><button class="home_button sv_btn">Nowe zgłoszenie</button></a> 
+            <a href="?page=repair.php" class="home_a"><button class="home_button sv_btn">Naprawiony</button></a>
+            <a href="?page=release.php" class="home_a"><button class="home_button sv_btn">Odbiór sprzętu</button></a>
         </div>
     </div>
 </main>
